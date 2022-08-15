f420f999e8188ddc29450a4c56609366482527ca
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-08-15 17:42:40 -0300

    Atualização

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/tabs/home_tab.dart';
+import 'package:lojavirtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +12,10 @@ class HomeScreen extends StatelessWidget {
     return PageView(
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        HomeTab()
+        Scaffold(
+         body: HomeTab(),
+         drawer: CustomDrawer(),
+        )
       ],
     );
   }

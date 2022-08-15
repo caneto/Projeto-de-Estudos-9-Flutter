0ad81c91a521c8c15e3183b13e8b6925c89b7997
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-08-15 10:05:14 -0300

    Iniciando aula 9

---
+++ android/app/src/main/kotlin/br/cap/sistema/lojavirtual/lojavirtual/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.cap.sistema.lojavirtual.lojavirtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/ui/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Loja Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}

82b0a18ad5b3bd34e24bf372795cf6287c1e564e
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-08-16 16:56:02 -0300

    Atualização

--- lib/ui/category_screen.dart
+++ lib/ui/category_screen.dart
@@ -2,13 +2,45 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatelessWidget {
-
   final DocumentSnapshot snapshot;
 
   CategoryScreen(this.snapshot, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(snapshot.get('title')),
+          centerTitle: true,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.grid_on),),
+              Tab(icon: Icon(Icons.list),)
+            ],
+          ),
+        ),
+        body: FutureBuilder<QuerySnapshot>(
+          future: FirebaseFirestore.instance
+              .collection("products").doc(snapshot.id).collection('itens')
+              .get(),
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  Container(color: Colors.red),
+                  Container(color: Colors.green)
+                ],
+              )
+            }
+          },
+        ),
+      ),
+    );
   }
 }

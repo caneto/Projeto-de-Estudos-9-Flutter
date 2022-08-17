65c9e1485d7d91f3c3030887eba1167105a9ebaa
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-08-17 09:16:05 -0300

    Atualização

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+
+  final String type;
+  final ProductData data;
+
+  ProductTile(this.type, this.data, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/ui/category_screen.dart
+++ lib/ui/category_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/datas/product_data.dart';
+import 'package:lojavirtual/tiles/product_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -27,16 +29,34 @@ class CategoryScreen extends StatelessWidget {
               .collection("products").doc(snapshot.id).collection('itens')
               .get(),
           builder: (context, snapshot) {
-            if(!snapshot.hasData) {
+            if (!snapshot.hasData) {
               return Center(child: CircularProgressIndicator());
             } else {
               return TabBarView(
                 physics: NeverScrollableScrollPhysics(),
                 children: [
-                  Container(color: Colors.red),
-                  Container(color: Colors.green)
+                  GridView.builder(
+                    padding: EdgeInsets.all(4.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                      childAspectRatio: 0.65,
+                    ),
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      return ProductTile("grid", ProductData.fromDocument(snapshot.data!.docs[index]));
+                    },
+                  ),
+                  ListView.builder(
+                    padding: EdgeInsets.all(4.0),
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        return ProductTile("list", ProductData.fromDocument(snapshot.data!.docs[index]));
+                      }
+                  )
                 ],
-              )
+              );
             }
           },
         ),

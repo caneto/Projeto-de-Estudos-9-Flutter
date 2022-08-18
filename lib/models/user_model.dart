90d1ee9d37fc3eec147c02b009413ccb5e70be0f
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-08-18 17:16:34 -0300

    atualização

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,11 +1,39 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
 
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? _user;
+  Map<String,dynamic> userData = Map();
+
   bool isLoading = false;
 
-  void signUp() {
+  void signUp(@required Map<String, dynamic> userData, @required String pass,
+      @required VoidCallback onSuccess, @required VoidCallback onFail) {
+    isLoading = true;
+    notifyListeners();
+
+    _auth.createUserWithEmailAndPassword(
+        email: userData['email'],
+        password: pass
+    ).then((user) async {
+      _user = user.user;
+
+      await _saveUserData(userData);
+
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e){
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
 
   }
 
@@ -26,4 +54,15 @@ class UserModel extends Model {
   //bool isLoggedIn() {
 //
  // }
+
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    .instance.collection("message").orderBy("time").snapshots(),
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(_user?.uid)
+        .set(userData);
+
+  }
+
 }
